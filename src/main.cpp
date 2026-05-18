200aacfba378881e1dabe4a47b4ef5b6b39df5c9
Avi <avishkarkale29@gmail.com>
2026-05-18 23:27:05 +0530

    Started depth buffering

--- src/main.cpp
+++ src/main.cpp
@@ -536,14 +536,15 @@ void createSwapChain(){
 
 }
 
-VkImageView createImageView(VkImage image , VkFormat format){
+VkImageView createImageView(VkImage image , VkFormat format ,VkImageAspectFlags aspectFlags){// added a new parameter
     VkImageViewCreateInfo viewInfo{};
     viewInfo.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO;
     viewInfo.image = image;
     viewInfo.viewType = VK_IMAGE_VIEW_TYPE_2D;
     viewInfo.format = format;
 
-    viewInfo.subresourceRange.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+    //viewInfo.subresourceRange.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+    viewInfo.subresourceRange.aspectMask = aspectFlags;
     viewInfo.subresourceRange.baseMipLevel = 0;
     viewInfo.subresourceRange.levelCount = 1;
     viewInfo.subresourceRange.baseArrayLayer = 0;
